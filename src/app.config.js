cc97f6d36634e46549d47a4e10501c547f7983cd
sam-littlefield <samuel.h.littlefield@github.com>
2023-11-08 23:38:34 +0000

    Changing team name again

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Team Zero';
+    team = 'Team Zero Fancy~';
     version = '1.0.0';
 }
 module.exports = AppConfig;
