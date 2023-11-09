e6f80e3300649f96644a41e8360224bc9712a1b9
sam-littlefield <samuel.h.littlefield@github.com>
2023-11-09 16:34:54 +0000

    script updates

--- src/services/database.service.js
+++ src/services/database.service.js
@@ -1,4 +1,30 @@
 const MockDatabaseService = require("./class.mock.database.service");
+const FirestoreDatabaseService = require("./class.firestore.database.service");
+
 let db;
-db = new MockDatabaseService();
+
+if(!!process.env.TESTING) {
+    db = new MockDatabaseService();
+    try {
+        db.populate().then(res => {
+            // console.log("Back from populate");
+            return true;
+        }).catch(err => {
+            console.log("Error from populate: ", err.message);
+        });
+    } catch(e) {
+        console.log(e.message);
+    }
+    
+} else {
+    const dbType = !!process.env.DB ? process.env.DB : 'Firestore';
+    if(dbType === 'Firestore') {
+        db = new FirestoreDatabaseService();
+    } else {
+        db = new MockDatabaseService();
+    }
+
+}
+
+
 module.exports = db;
